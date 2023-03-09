16f0f5536e802636a0d7cf252f31e04299c2c584
Thammy F. Vargas A <thammyfva@hotmail.com>
2023-03-09 16:32:34 -0400

    adding some views

--- GestionInventario/GestionInventarioApp/urls.py
+++ GestionInventario/GestionInventarioApp/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [ 
-    path('',views.main,name='mainpage')]
+    path('',views.main,name='mainpage'),
+    path('categoria/',views.categoria,name='categoria'),
+    path('price/',views.precio,name='precio')
+    ]
--- GestionInventario/GestionInventarioApp/views.py
+++ GestionInventario/GestionInventarioApp/views.py
@@ -4,3 +4,11 @@ from . import models
 def main(request):
     data = models.Producto.objects.all()
     return render(request,'Gestion/gestion.html',{'data':data})
+
+def categoria(request):
+    data = models.Categoria.objects.filter().order_by('name')
+    return render (request,'Gestion/gestion.html',{'data':data})
+
+def precio(request):
+    data = models.Producto.objects.filter().order_by('price')
+    return render (request,'Gestion/gestion.html',{'data':data})
